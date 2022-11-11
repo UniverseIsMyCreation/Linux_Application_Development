50887c572ae20763184ae4a244e7d6eb59db35f1
UniverseIsMyCreation <UniverseIsMyCreation@yandex.com>
2022-11-11 18:41:33 +0300

    add tests

--- 08_Environmental/simple_test.sh
+++ 08_Environmental/simple_test.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-if [ "$(echo -n "tstnum1" | sha1sum | cut -f 1 -d " ")" != "$(echo -n "SHA1 \"tstnum1" | ./rhasher)" ]; then
+res1=$(echo -n "tstnum1" | sha1sum | cut -f 1 -d " ")
+res2=$(echo -n "SHA1 \"tstnum1" | ./rhasher)
+if [ "echo $res1" != "echo $res2" ]; then
     echo "SHA1 TEST 1: NOT OK"
 else
     echo "SHA1 TEST 1: OK"
 fi
 
-if [ "$(echo -n "bbbbbbbbbbbbb" | sha1sum | cut -f 1 -d " ")" != "$(echo -n "SHA1 \"bbbbbbbbbbbbb" | ./rhasher)" ]; then
+res1=$(echo -n "bbbbbbbbbbbbb" | sha1sum | cut -f 1 -d " ")
+res2=$(echo -n "SHA1 \"bbbbbbbbbbbbb" | ./rhasher)
+if [ "echo $res1" != "echo $res2" ]; then
     echo "SHA1 TEST 2: NOT OK"
 else
     echo "SHA1 TEST 2: OK"
 fi
 
-if [ "$(echo -n "tstnumtwo" | md5sum | cut -f 1 -d " ")" != "$(echo -n "MD5 \"tstnumtwo" | ./rhasher)" ]; then
+res1=$(echo -n "tstnumtwo" | md5sum | cut -f 1 -d " ")
+res2=$(echo -n "MD5 \"tstnumtwo" | ./rhasher)
+if [ "echo $res1" != "echo $res2" ]; then
     echo "MD5 TEST 1: NOT OK"
 else
     echo "MD5 TEST 1: OK"
 fi
 
-if [ "$(echo -n "asdasdasd" | md5sum | cut -f 1 -d " ")" != "$(echo -n "MD5 \"asdasdasd" | ./rhasher)" ]; then
+res1=$(echo -n "asdasdasd" | md5sum | cut -f 1 -d " ")
+res2=$(echo -n "MD5 \"asdasdasd" | ./rhasher)
+if [ "echo $res1" != "echo $res2" ]; then
     echo "MD5 TEST 2: NOT OK"
 else
     echo "MD5 TEST 2: OK"
